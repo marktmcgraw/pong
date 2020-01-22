01f627bdf728321ee18f3d6f22ed1f7cec8223fe
Mark McGraw <mtmcgr@gmail.com>
2020-01-22 14:26:00 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Paddle from "./components/Paddle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Paddle />
+    </div>
+  );
+}
