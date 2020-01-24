c9ee2d9abf53d125263dc878ba4e1ae0bcfc2e96
Mark McGraw <mtmcgr@gmail.com>
2020-01-24 11:16:47 -0600

    updated to useReducer
    in app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./styles.css";
 import Paddle from "./components/Paddle";
-import Ball from "./components/Ball";
+
+const initialState = {
+  paddle1: {
+    y: 0
+  },
+  paddle2: {
+    y: 0
+  }
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "MOVE_PADDLE_1":
+      return { ...state, paddle1: action.payload };
+    case "MOVE_PADDLE_2":
+      return { ...state, paddle2: action.payload };
+    default:
+      throw new Error();
+  }
+}
 
 export default function App() {
-  const [p1PaddleY, setP1PaddleY] = useState(0);
-  const [p2PaddleY, setP2PaddleY] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   function handleKey(e) {
     const char = e.key.toLowerCase();
     if (char === "w" || char === "s") {
-      setP1PaddleY(p1PaddleY + (char === "w" ? -10 : 10));
+      dispatch({
+        type: "MOVE_PADDLE_1",
+        payload: {
+          y: state.paddle1.y + (char === "w" ? -10 : 10)
+        }
+      });
     }
     if (char === "o" || char === "l") {
-      setP2PaddleY(p2PaddleY + (char === "o" ? -10 : 10));
+      dispatch({
+        type: "MOVE_PADDLE_2",
+        payload: {
+          y: state.paddle2.y + (char === "o" ? -10 : 10)
+        }
+      });
     }
   }
   useEffect(() => {
     window.addEventListener("keydown", handleKey);
     return () => window.removeEventListener("keydown", handleKey);
-  }, [p1PaddleY, p2PaddleY]);
+  }, [state]);
   return (
     <div className="container">
-      <Paddle paddleY={p1PaddleY} />
-      <Paddle isPlayerTwo paddleY={p2PaddleY} />
-      <Ball />
+      <Paddle paddleY={state.paddle1.y} />
+      <Paddle isPlayerTwo paddleY={state.paddle2.y} />
     </div>
   );
 }
+
+// export default function App() {
+//   const [p1PaddleY, setP1PaddleY] = useState(0);
+//   const [p2PaddleY, setP2PaddleY] = useState(0);
+
+//   function handleKey(e) {
+//     const char = e.key.toLowerCase();
+//     if (char === "w" || char === "s") {
+//       setP1PaddleY(p1PaddleY + (char === "w" ? -10 : 10));
+//     }
+//     if (char === "o" || char === "l") {
+//       setP2PaddleY(p2PaddleY + (char === "o" ? -10 : 10));
+//     }
+//   }
+//   useEffect(() => {
+//     window.addEventListener("keydown", handleKey);
+//     return () => window.removeEventListener("keydown", handleKey);
+//   }, [p1PaddleY, p2PaddleY]);
+//   return (
+//     <div className="container">
+//       <Paddle paddleY={p1PaddleY} />
+//       <Paddle isPlayerTwo paddleY={p2PaddleY} />
+//       <Ball />
+//     </div>
+//   );
+// }
